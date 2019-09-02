47ebf272567a28e7fb613b7cef3bc00b1b466736
Arjun Chauhan <arjun99.chauhan@gmail.com>
2019-09-02 13:08:22 +0530

    added all the files

---
+++ precache-manifest.93b06fb36c32e1fc79a1004e50c549d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07891bc1cf880699bccb869636deb357",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4c405cc663d541fad1d",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "af460f829abab5b6fa8f",
+    "url": "/static/css/main.96b2ae2f.chunk.css"
+  },
+  {
+    "revision": "a4c405cc663d541fad1d",
+    "url": "/static/js/2.52355532.chunk.js"
+  },
+  {
+    "revision": "af460f829abab5b6fa8f",
+    "url": "/static/js/main.df6baa64.chunk.js"
+  },
+  {
+    "revision": "31175d55b7ad255ef221",
+    "url": "/static/js/runtime~main.6237261c.js"
+  }
+]);
